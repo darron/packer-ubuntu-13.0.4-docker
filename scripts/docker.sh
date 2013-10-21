9b6e5df6e8a2736ff7f3d18ef2b382ddfe319893
darron froese <darron@froese.org>
2013-10-20 20:01:39 -0600

    Update from current instructions.
    From http://docs.docker.io/en/latest/installation/ubuntulinux/#ubuntu-raring

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # Add the Docker repository to your apt sources list.
-sudo apt-get install curl
-sudo sh -c "curl http://get.docker.io/gpg | apt-key add -"
-sudo sh -c "echo deb https://get.docker.io/ubuntu docker main > /etc/apt/sources.list.d/docker.list"
+sudo apt-get install curl wget
+sudo sh -c "wget -qO- https://get.docker.io/gpg | apt-key add -"
+sudo sh -c "echo deb http://get.docker.io/ubuntu docker main > /etc/apt/sources.list.d/docker.list"
 
 # Update your sources
 sudo apt-get update
 
-# Install, you will see another warning that the package cannot be authenticated. Confirm install.
-sudo apt-get install -y --force-yes lxc-docker
+# Install
+sudo apt-get install lxc-docker
